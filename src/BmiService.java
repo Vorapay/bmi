eab107104395a9a5741ba8b78431ec8759c1f720
Vara <artemiivoropaev@gmail.com>
2025-02-16 20:09:56 +0700

    new commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(double heightMeter, int weightKilogram) {
-        int v=(int) (weightKilogram/(heightMeter*heightMeter));
-        return v;
+        int bodyMassIndex = (int) (weightKilogram / (heightMeter * heightMeter));
+        return bodyMassIndex;
     }
 }
